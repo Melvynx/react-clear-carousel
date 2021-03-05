3fd69de33cf66538c3df22a652f629ac3ccdbfa7
Melvynx <melvyn.malherbe@qoqa.com>
2021-03-05 09:28:01 +0100

    Updates

---
+++ precache-manifest.f3dbf8a0a3d8df7804c9973748f455c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8d697759b28c310c2380b5955460beb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f42aed183f7df9e4411f",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "f404971ae35431b7d1c6",
+    "url": "./static/js/2.f2a509c2.chunk.js"
+  },
+  {
+    "revision": "df4964ae36e0e724ce02a12cce603843",
+    "url": "./static/js/2.f2a509c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f42aed183f7df9e4411f",
+    "url": "./static/js/main.3d987afa.chunk.js"
+  },
+  {
+    "revision": "0a034d4dd669abe50f05",
+    "url": "./static/js/runtime-main.5e09c831.js"
+  }
+]);
